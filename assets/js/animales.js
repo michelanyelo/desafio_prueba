15b998657f74cb02999bdd8b99cb9c07338c5f53
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-08-13 19:54:04 -0400

    M5D8 prueba: Added Lobo class extending Animal with constructor and Aullar method

--- assets/js/animales.js
+++ assets/js/animales.js
@@ -45,3 +45,13 @@ class Leon extends Animal {
     }
 }
 
+class Lobo extends Animal {
+    constructor(nombre, edad, img, comentarios, sonido) {
+        super(nombre, edad, img, comentarios, sonido);
+    }
+
+    Aullar() {
+        console.log(this.Sonido);
+    }
+}
+
