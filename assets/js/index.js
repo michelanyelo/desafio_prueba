1a1178ad7b5677fd6a98cea192b3a25c21d8bebd
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-08-15 19:48:07 -0400

    M5D8 prueba: Updated animales.json and index.js: changed sonido filename, updated rutaBaseImg and rutaBaseSonido, and added image preview and audio playback functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,36 +13,62 @@ document.addEventListener("DOMContentLoaded", () => {
         const comentariosAnimal = formularioAnimales.comentarios.value;
 
         // Ruta base de img y sonido
-        const rutaBaseImg = "../imgs/";
-        const rutaBaseSonido = "../sounds/";
+        const rutaBaseImg = "assets/imgs/";
+        const rutaBaseSonido = "assets/sounds/";
         // Buscar imagen y sonido usando buscarAnimal
-        const { imagen, sonido } = await buscarAnimal(nombreAnimal);
+        let { imagen, sonido } = await buscarAnimal(nombreAnimal);
+
+        imagen = rutaBaseImg + imagen;
+        sonido = rutaBaseSonido + sonido;
 
         // instancia de la clase
         let animal = null;
 
         switch (nombreAnimal) {
             case "Leon":
-                animal = new Leon(nombreAnimal, edadAnimal, rutaBaseImg + imagen, comentariosAnimal, rutaBaseSonido + sonido);
+                animal = new Leon(nombreAnimal, edadAnimal, imagen, comentariosAnimal, sonido);
                 break;
             case "Lobo":
-                animal = new Lobo(nombreAnimal, edadAnimal, rutaBaseImg + imagen, comentariosAnimal, rutaBaseSonido + sonido);
+                animal = new Lobo(nombreAnimal, edadAnimal, imagen, comentariosAnimal, sonido);
                 break;
             case "Oso":
-                animal = new Oso(nombreAnimal, edadAnimal, rutaBaseImg + imagen, comentariosAnimal, rutaBaseSonido + sonido);
+                animal = new Oso(nombreAnimal, edadAnimal, imagen, comentariosAnimal, sonido);
                 break;
             case "Serpiente":
-                animal = new Serpiente(nombreAnimal, edadAnimal, rutaBaseImg + imagen, comentariosAnimal, rutaBaseSonido + sonido);
+                animal = new Serpiente(nombreAnimal, edadAnimal, imagen, comentariosAnimal, sonido);
                 break;
             case "Aguila":
-                animal = new Aguila(nombreAnimal, edadAnimal, rutaBaseImg + imagen, comentariosAnimal, rutaBaseSonido + sonido);
+                animal = new Aguila(nombreAnimal, edadAnimal, imagen, comentariosAnimal, sonido);
                 break;
             default:
                 alert("Animal no encontrado");
         }
 
         console.log(animal);
+
+        // Vista previa de la imagen
+        const imgHtml = document.createElement('img');
+        imgHtml.className = 'img-fluid';
+        imgHtml.src = `${imagen}`;
+        document.getElementById('preview').append(imgHtml);
+
+        let containerAnimales = document.getElementById('Animales');
+        containerAnimales.innerHTML += `
+                    <div class="card h-100 mx-2" style="width: 18rem;">
+            <img src="${imagen}" class="card-img-top img-fluid" alt="${nombreAnimal} en investigación">
+            <div class="card-body">
+                <audio controls style="max-width: 100%"> <source src="${sonido}" type="audio/mpeg">Reproducir sonido</audio>
+            </div>
+            </div>
+        `;
+
+        // Estado inicial del formulario
+        setTimeout(() => {
+            document.forms.animalesForm.reset();
+            document.getElementById('preview').innerHTML = '';
+        }, 1000);
     });
+
 
 
     const buscarAnimal = async (nombreAnimal) => {
